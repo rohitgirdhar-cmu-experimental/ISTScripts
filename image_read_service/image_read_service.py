ac688457227079fd9e18e45cea9573ebfae79744
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-06-06 20:31:10 +0000

    img read service added

---
+++ image_read_service/image_read_service.py
@@ -0,0 +1,27 @@
+import zmq
+import happybase
+import base64
+
+port_num = 5554
+DEBUG = False
+
+# set up the hbase access
+conn = happybase.Connection('10.1.94.57')
+tab = conn.table('roxyscrape')
+
+# set up the server
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:%s" % port_num)
+print 'Server Ready at %d' % port_num
+
+while True:
+  imid = socket.recv()
+  if DEBUG:
+    print 'Received %s' % imid
+  try:
+    img = base64.b64decode(tab.row(imid)['image:orig'])
+  except:
+    img = r''
+  socket.send(img)
+
